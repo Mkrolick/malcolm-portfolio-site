2ebdf02d643ecb46e1d9d58c6a413d347dc72d8d
Malcolm Krolick <malcolm.krolick@gmail.com>
2024-09-21 17:45:45 -0400

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -78,10 +78,6 @@ const siteConfig: SiteConfig = {
             text: 'Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
     ],
     socialLinks: [
         {
